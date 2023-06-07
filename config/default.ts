e67e992d16ecf167f81bebfb4945e84f46c39160
lwinmin-12 <lmo.lwinmin@gmail.com>
2023-06-07 15:54:02 +0630

    after_simple

---
+++ config/default.ts
@@ -0,0 +1,8 @@
+export default {
+  port: 9000,
+  host: "localhost",
+  dbUrl: "mongodb://localhost:27017/fuel-station",
+  saltWorkFactor: 10,
+  secretKey: "suuhh",
+  page_limit : 2,
+};
---
+++ src/router/detailSale.routes.ts
@@ -0,0 +1,42 @@
+import {
+  addDetailSaleHandler,
+  deleteDetailSaleHandler,
+  detailSalePaginateHandler,
+  getDetailSaleHandler,
+  updateDetailSaleHandler,
+} from "../controller/detailSale.controller";
+import { hasAnyPermit } from "../middleware/permitValidator";
+import { roleValidator } from "../middleware/roleValidator";
+import { validateAll, validateToken } from "../middleware/validator";
+import { allSchemaId, detailSaleSchema } from "../schema/scheama";
+
+const detailSaleRoute = require("express").Router();
+
+detailSaleRoute.get(
+  "/",
+  validateToken,
+  hasAnyPermit(["view"]),
+  getDetailSaleHandler
+);
+
+//that for only device
+detailSaleRoute.post("/", addDetailSaleHandler);
+detailSaleRoute.patch("/", updateDetailSaleHandler);
+
+detailSaleRoute.delete(
+  "/",
+  validateToken,
+  roleValidator("admin"),
+  hasAnyPermit(["delete"]),
+  validateAll(allSchemaId),
+  deleteDetailSaleHandler
+);
+
+detailSaleRoute.get(
+  "/pagi/:page",
+  // validateToken,
+  // hasAnyPermit(["view"]),
+  detailSalePaginateHandler
+);
+
+export default detailSaleRoute;
